2c46b4afe1efd772f7992e74b4bba81bd9cd6335
Marcus Paulo Oliveira <47436367+MarcusPOliveira@users.noreply.github.com>
2024-02-04 17:32:45 -0300

    feat: checkbox component

---
+++ src/components/checkbox/index.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { motion } from 'framer-motion'
+
+type CheckboxProps = {
+  checked?: boolean
+  setChecked?: (checked: boolean) => void
+}
+
+export const Checkbox = ({ checked, setChecked }: CheckboxProps) => {
+  return (
+    <motion.label className="flex items-center" whileTap={{ scale: 0.85 }}>
+      <input
+        type="checkbox"
+        className="hidden"
+        checked={checked}
+        onChange={() => setChecked && setChecked(!checked)}
+      />
+      <span
+        className={`flex h-10 w-10 items-center justify-center rounded-full bg-white p-1`}
+      >
+        {checked ? (
+          <motion.span
+            className="h-6 w-6 rounded-full bg-emerald-500"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.4 }}
+          />
+        ) : null}
+      </span>
+    </motion.label>
+  )
+}
