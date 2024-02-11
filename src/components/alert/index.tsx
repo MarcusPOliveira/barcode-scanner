abb934a0b3db93a89ca64f0d915c0b8ac0cdd3b6
Marcus Paulo Oliveira <47436367+MarcusPOliveira@users.noreply.github.com>
2024-02-11 00:31:40 -0300

    feat: alert component with sweetalert2

---
+++ src/components/alert/index.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { useEffect } from 'react'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+type AlertProps = {
+  title: string
+  message: string
+  isOpen: boolean
+  handleConfirm?: () => void
+  handleCancel?: () => void
+}
+
+export const Alert = ({
+  title,
+  message,
+  isOpen,
+  handleConfirm,
+  handleCancel,
+}: AlertProps) => {
+  const MySwal = withReactContent(Swal)
+
+  useEffect(() => {
+    if (isOpen) {
+      MySwal.fire({
+        title: <p>{title}</p>,
+        html: message,
+        showCloseButton: true,
+        showCancelButton: true,
+        showConfirmButton: true,
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar',
+      }).then((r) => {
+        if (r.isConfirmed) {
+          handleConfirm && handleConfirm()
+        } else {
+          handleCancel && handleCancel()
+        }
+      })
+    }
+  }, [isOpen])
+
+  return null
+}
