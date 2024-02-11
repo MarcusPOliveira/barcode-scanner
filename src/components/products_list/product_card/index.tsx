1c7c6bd14ba0bb48a235d24fc843e02b00664934
Marcus Paulo Oliveira <47436367+MarcusPOliveira@users.noreply.github.com>
2024-02-11 00:32:02 -0300

    feat: swipe to delete animation

--- src/components/products_list/product_card/index.tsx
+++ src/components/products_list/product_card/index.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
-import { Clock } from 'lucide-react'
+import { Clock, Trash2 } from 'lucide-react'
 
-import { Checkbox } from '@/components'
 import { formatDatetime } from '@/utils'
+
+import { Alert, Checkbox } from '@/components'
 
 type ProductCardProps = {
   id: string
@@ -19,12 +21,53 @@ export const ProductCard = ({
   isSelected = false,
   setIsSelected,
 }: ProductCardProps) => {
+  const [isAct, setIsAct] = useState(0)
+  const [showAct, setShowAct] = useState(true)
+  const [isOpenAlert, setIsOpenAlert] = useState(false)
+
   const { time } = formatDatetime(datetime)
 
+  const variantShowAct = {
+    hidden: {
+      opacity: 0,
+      width: 0,
+    },
+    show: {
+      opacity: 1,
+      width: 'auto',
+    },
+  }
+
+  const dragAnimation = {
+    show: {
+      translateX: 0,
+    },
+    hidden: {
+      translateX: -60,
+    },
+  }
+
+  const handleDeleteItem = () => {
+    console.log('Apagando item')
+  }
+
+  console.log('showAct', showAct)
+
+  useEffect(() => {
+    setShowAct(!showAct)
+    // eslint-disable-next-line
+  }, [isAct])
+
   return (
-    <div
+    <motion.div
       key={`product_card_${id}_${ean}_${datetime}`}
       className="flex min-h-[80px] w-full justify-between rounded-2xl px-4 odd:bg-slate-300 even:bg-slate-200"
+      drag="x"
+      dragConstraints={{ left: -10, right: 0, top: 0, bottom: 0 }}
+      variants={dragAnimation}
+      animate={showAct ? 'hidden' : 'show'}
+      onDirectionLock={(axis) => console.log(axis)}
+      onDragEnd={(event, info) => setIsAct(info.point.x)}
     >
       <div className="flex items-center gap-4">
         <Checkbox checked={isSelected} setChecked={setIsSelected} />
@@ -35,6 +78,29 @@ export const ProductCard = ({
         <Clock size={16} className="opacity-60" />
         <p className="opacity-70">{time}</p>
       </div>
-    </div>
+      <motion.div
+        className="absolute right-0 top-0 flex min-h-[80px] w-full flex-row-reverse overflow-hidden rounded-xl opacity-0"
+        variants={variantShowAct}
+        animate={showAct ? 'show' : 'hidden'}
+      >
+        <motion.button
+          type="button"
+          className="flex h-20 w-20 items-center justify-center bg-red-500"
+          onClick={() => setIsOpenAlert(true)}
+        >
+          <Trash2 size={24} color="white" />
+        </motion.button>
+      </motion.div>
+      <Alert
+        title="Apagar"
+        message="Tem certeza que deseja apagar esse item?"
+        isOpen={isOpenAlert}
+        handleConfirm={() => handleDeleteItem()}
+        handleCancel={() => {
+          setShowAct(!showAct)
+          setIsOpenAlert(false)
+        }}
+      />
+    </motion.div>
   )
 }
